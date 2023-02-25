602d3e2e42fa8d5076abce370f0367db3c6505a2
Gabriele Ranzieri <104820500+Ranzeb@users.noreply.github.com>
2023-02-25 17:21:45 +0100

    Bug fix on InputType emit types

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import { z } from 'zod';
 import { ObjectId } from 'bson';
-//import NodeDataType from './schema/nodeDataType';
+import NodeDataType from './schema/nodeDataType';
 
 const hexString = "be5d55129807e725bda1b061";
 
-/*
 type correctType = z.infer<typeof NodeDataType>
 type correctInputType = z.input<typeof NodeDataType>
 type correctOutputType = z.output<typeof NodeDataType>
-
 
 const actualNodeDataFromObjectId = NodeDataType.parse({
   _id: ObjectId.createFromHexString(hexString)
@@ -20,59 +18,4 @@ const actualNodeDataFromString = NodeDataType.parse({
 
 console.log(actualNodeDataFromObjectId);
 console.log(actualNodeDataFromString);
-*/
 
-const myUnion = z.discriminatedUnion("status", [
-  z.object({ status: z.literal("success"), data: z.string() }),
-  z.object({ status: z.literal("failed"), error: z.instanceof(Error) }),
-]);
-
-
-const checkString = z.optional(z.string().min(24).transform((input, ctx) => {
-  try {
-    return ObjectId.createFromHexString(input);
-  } catch (error) {
-    ctx.addIssue({
-      message: error.message,
-      code: z.ZodIssueCode.custom,
-      params: { input },
-    })
-  }
-})).optional();
-
-const checkObjectId = z.instanceof(ObjectId).optional();
-
-const NodeDataType = z.union([checkString, checkObjectId]);
-
-const PreprocessType = z.preprocess((arg) => {
-  if (typeof arg == "string") return ObjectId.createFromHexString(arg)
-  else return arg;
-}, z.instanceof(ObjectId).optional())
-
-const FinalDataType: z.ZodType<ObjectId | undefined, z.ZodTypeDef, ObjectId | undefined> = z.object({
-  _id: PreprocessType
-})
-
-type correctPreprocessTypeInput = z.input<typeof FinalDataType>
-type correctPreprocessType = z.infer<typeof FinalDataType>
-type correctPreprocessTypeOutput = z.output<typeof FinalDataType>
-
-const NodeDataUpdatedType = z.object({
-  _id: z.instanceof(ObjectId).optional()
-})
-
-type correctType = z.infer<typeof NodeDataType>
-type correctInputType = z.input<typeof NodeDataType>
-type correctOutputType = z.output<typeof NodeDataUpdatedType>
-
-
-const actualNodeDataFromObjectId = FinalDataType.parse({
-  _id: ObjectId.createFromHexString(hexString)
-})
-
-const actualNodeDataFromString = FinalDataType.parse({
-  _id: hexString
-})
-
-console.log("objectId: " + actualNodeDataFromObjectId);
-console.log("hexString: " + actualNodeDataFromString);
--- src/schema/nodeDataType.ts
+++ src/schema/nodeDataType.ts
@@ -6,18 +6,16 @@ import { ObjectId } from 'bson';
 // The schema will also check in advance that the string is at least 24 characters long because we already know that's not a valid hex string. 
 // It should be at least 24 characters or 12 bytes long
 
+const PreProcessId =
+  z.preprocess((arg) => {
+    if (typeof arg == "string" && arg.length >= 24) return ObjectId.createFromHexString(arg)
+    else if(arg instanceof ObjectId) return arg;
+    else return undefined;
+  }, z.instanceof(ObjectId).optional()) as z.ZodEffects<z.ZodTypeAny, ObjectId | undefined, ObjectId | undefined>;
+
+
 const NodeDataType = z.object({
-    _id: z.instanceof(ObjectId).optional().or(z.string().min(24).transform((input, ctx) => {
-        try {
-            return ObjectId.createFromHexString(input);
-        } catch (error) {
-            ctx.addIssue({
-                message: error.message,
-                code: z.ZodIssueCode.custom,
-                params: { input },
-            })
-        }
-    }))
-})
+  _id: PreProcessId
+});
 
 export default NodeDataType;
